ac0c3053341570dcf7076642ac93043f717a4a28
= <=>
2022-08-22 20:16:14 -0300

    Segunda Alteração

--- core/admin.py
+++ core/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 
-from core.models import Categoria
+from core.models import Autor, Categoria, Editora, Livro
 
 admin.site.register(Categoria)
+admin.site.register(Editora)
+admin.site.register(Livro)
+admin.site.register(Autor)
